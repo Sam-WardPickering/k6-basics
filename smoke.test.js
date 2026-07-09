46d9ca469faf38895c8c89bb7b3a00fcb639d64e
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-09 17:00:06 +1200

    Add smoke test for practicesoftwaretesting.com

---
+++ smoke.test.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    vus: 1,
+    duration: '10s',
+};
+
+export default function () {
+    const response = http.get('https://api.practicesoftwaretesting.com/products');
+
+    check(response, {
+        'status is 200': (r) => r.status === 2000,
+        'response time < 500ms': (r) => r.timings.duration < 500,
+    });
+};
