aa815aed15202787ce5123a2dbfbdab0232dbd2f
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-13 16:49:37 +1200

    Refactor label

--- smoke.test.js
+++ smoke.test.js
@@ -13,6 +13,6 @@ export default function () {
 
     check(response, {
         'status is 200': (r) => r.status === 200,
-        'response time < 500ms': (r) => r.timings.duration < 1000,
+        'response time < 1000ms': (r) => r.timings.duration < 1000,
     });
 };
